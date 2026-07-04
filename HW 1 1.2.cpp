1f78f9dfda38ec009ba596e12d768481d893a5a9
mongkonmeeaumnat <mongkonppap09@gmail.com>
2026-07-04 09:19:40 +0700

    Update HW 1 1.2.cpp

--- HW 1 1.2.cpp
+++ HW 1 1.2.cpp
@@ -8,14 +8,16 @@ double F(double x) ;
 int main() {
 	int n ; printf("Input Number of range\n");
 	scanf("%d",&n) ;
-	if(n > 10001) {
-    printf("Maximum n is 10001\n");
-    return 1;
-    
+	if(n > 10000) {
+    	printf("Maximum n is 10000\n");
+    	return 1;
+    	
 	} else {
-		double a , b , x[10001] , fx[10001] ;
+		double a , b , x[10001] , fx[10001] , ex ;
 	printf("Input Value of x(0) and x(n)\n") ;
 	scanf("%lf %lf",&a,&b);
+	printf("Input Exact Value\n") ;
+	scanf("%lf",&ex) ;
 	double h = (b - a) / n ;
 	for(int i=0;i<=n;i++) {
 		x[i] = a + i*h ; // compute x(i) ;
@@ -25,15 +27,14 @@ int main() {
 	}
 	double sum = 0 ; // compute 2*(fx(1) to fx (n-1) ) ;
 	for(int i=1;i<n;i++) {
-		sum += 2 * (fx[i]) ;
+		sum += 2 * fx[i] ;
 	}
 	double I = h/2.0 * (fx[0] + fx[n] + sum ) ;
 	printf("I = %lf\n",I);
 	
-	double e = ( ( 155930.4 - I ) / 155930.4 ) * 100;
+	double e = ( ( ex - I ) / ex ) * 100;
 	printf("Error is %lf percentage",e) ;
 }
-
 	
 	
 }
